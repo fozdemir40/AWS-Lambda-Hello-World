fb56cd7cf4890d1727de0ec6403b6cb9ad366c14
Furkan Ozdemir <f.ozdemir40@hotmail.com>
2022-03-04 13:24:17 +0100

    Restore files

--- main.tf
+++ main.tf
@@ -1,34 +1,38 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.2.0"
-    }
-  }
+# The following configuration uses a provider which provisions [fake] resources
+# to a fictitious cloud vendor called "Fake Web Services". Configuration for
+# the fakewebservices provider can be found in provider.tf.
+#
+# After running the setup script (./scripts/setup.sh), feel free to change these
+# resources and 'terraform apply' as much as you'd like! These resources are
+# purely for demonstration and created in Terraform Cloud, scoped to your TFC
+# user account.
+#
+# To review the provider and documentation for the available resources and
+# schemas, see: https://registry.terraform.io/providers/hashicorp/fakewebservices
+#
+# If you're looking for the configuration for the remote backend, you can find that
+# in backend.tf.
 
-  required_version = "~> 1.0"
+
+resource "fakewebservices_vpc" "primary_vpc" {
+  name       = "Primary VPC"
+  cidr_block = "0.0.0.0/1"
 }
 
-provider "aws" {
-  region = var.aws_region
+resource "fakewebservices_server" "servers" {
+  count = 2
+
+  name = "Server ${count.index + 1}"
+  type = "t2.micro"
+  vpc  = fakewebservices_vpc.primary_vpc.name
 }
 
-resource "random_pet" "lambda_bucket_name" {
-  prefix = "learn-terraform-functions"
-  length = 4
+resource "fakewebservices_load_balancer" "primary_lb" {
+  name    = "Primary Load Balancer"
+  servers = fakewebservices_server.servers[*].name
 }
 
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = random_pet.lambda_bucket_name.id
-
-  acl           = "private"
-  force_destroy = true
+resource "fakewebservices_database" "prod_db" {
+  name = "Production DB"
+  size = 256
 }
