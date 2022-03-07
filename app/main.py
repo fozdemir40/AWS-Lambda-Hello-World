dad656c74db2425dba8e63b15346e1df510687a0
Furkan Ozdemir <f.ozdemir40@hotmail.com>
2022-03-07 09:00:12 +0100

    Added changable return message in handler

--- app/main.py
+++ app/main.py
@@ -2,9 +2,13 @@ import json
 
 
 def hello(event, context):
+    response_message = 'Hello, world!'
+    if event['queryStringParameters']:
+        name_from_query_parameter = event['queryStringParameters']['Name']
+        response_message = f'Hello, {name_from_query_parameter}!'
+
     body = {
-        "message": "Hello world!",
-        "input": event
+        "message": response_message,
     }
 
     return {"statusCode": 200, "body": json.dumps(body)}
